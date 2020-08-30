29481fd699e085ce9f445b0ae905e5459b077356
dthecsguy <50031416+dthecsguy@users.noreply.github.com>
2020-08-29 17:55:57 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -14,10 +14,12 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00;    PORTA = 0xFF;
+    DDRB = 0xFF;    PORTB = 0x00;
+    
     /* Insert your solution below */
     while (1) {
-
+        PORTB = 0xFF;
     }
     return 1;
 }
